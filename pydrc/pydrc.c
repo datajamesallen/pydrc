9eb4ef0d5bd805132575ab5be96b0bdbec9c7341
James Allen <jpatallen@protonmail.com>
2019-09-27 09:13:13 -0400

    fit4pdrc test with a preset list of doeses with a list of responses as the argument

--- pydrc/pydrc.c
+++ pydrc/pydrc.c
@@ -4,10 +4,36 @@
 
 // function
 static PyObject* fit4pdrc(PyObject* self, PyObject* args) {
-	char * input;
-	double * res;
+	// first test with a list of responses for a set doses list
+	PyObject *pList;
+	PyObject *pItem;
+	Py_ssize_t n;
+	int i;
+
+	if (!PyArg_ParseTuple(args, "O!", &PyList_Type, &pList)) {
+    	PyErr_SetString(PyExc_TypeError, "parameter must be a list.");
+    	return NULL;
+	}
+
+	n = PyList_Size(pList);
+	for (i=0; i<n; i++) {
+		pItem = PyList_GetItem(pList, i);
+		if(!PyFloat_Check(pItem)) {
+			PyErr_SetString(PyExc_TypeError, "list items must be floats.");
+        	return NULL;
+    	}
+	}
 	printf("test\n");
-	return Py_None;
+	Py_INCREF(Py_None);
+	ret = Py_None;
+	assert(! PyErr_Occurred());
+	assert(ret)
+	goto finally;
+except:
+	Py_XDECREF(ret)
+	ret = NULL;
+finally:
+	return ret;
 }
 
 // function Definition struct
