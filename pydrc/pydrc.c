7a84d27bfb898c68879761dfa1f1d70fe6ebbf05
James Allen <jpatallen@protonmail.com>
2019-11-22 14:53:14 -0500

    added top and bot constrained methods to 0 and 100

--- pydrc/pydrc.c
+++ pydrc/pydrc.c
@@ -69,9 +69,214 @@ static PyObject* fit4pdrc(PyObject* self, PyObject* args) {
 	return ret_dict;
 }
 
+// COPY OF fit4pdrc but with fit2d
+static PyObject* top_const_fit4pdrc(PyObject* self, PyObject* args) {
+	// first test with a list of responses for a set doses list
+	PyObject *doseList;
+	PyObject *doseItem;
+	PyObject *resList;
+	PyObject *resItem;
+	Py_ssize_t n;
+	Py_ssize_t dose_n;
+	Py_ssize_t res_n;
+	int i;
+
+	if (!PyArg_ParseTuple(args, "O!O!", &PyList_Type, &doseList, &PyList_Type, &resList)) {
+    	PyErr_SetString(PyExc_TypeError, "parameter must be a list of doses, followed by a list of responses.");
+    	return NULL;
+	}
+	dose_n = PyList_Size(doseList);
+	res_n = PyList_Size(resList);
+	if (dose_n != res_n) {
+		PyErr_SetString(PyExc_ValueError, "the dose and response list must be of equal size");
+		return NULL;
+	}
+	n = dose_n;
+	double * dose = malloc(sizeof(double) * dose_n);
+	double * res = malloc(sizeof(double) * res_n);
+	for (i=0; i<n; i++) {
+		doseItem = PyList_GetItem(doseList, i);
+		resItem = PyList_GetItem(resList, i);
+
+		if(PyFloat_Check(resItem)) {
+			res[i] = PyFloat_AsDouble(resItem);
+		} else if(PyLong_Check(resItem)) {
+			res[i] = PyFloat_AsDouble(resItem);
+		//} else if(resItem == Py_None) {
+		//	continue;
+		} else {
+			PyErr_SetString(PyExc_TypeError, "list items must be numeric.");
+        	return NULL;
+		}
+		if(PyFloat_Check(doseItem)) {
+			dose[i] = PyFloat_AsDouble(doseItem);
+		} else if(PyLong_Check(doseItem)) {
+			dose[i] = PyLong_AsDouble(doseItem);
+		} else {
+			PyErr_SetString(PyExc_TypeError, "list items must be numeric.");
+        	return NULL;
+		}
+	}
+	double ret_arr[6];
+	fit2d(res, dose, n, ret_arr);
+	free(res);
+	free(dose);
+	/*
+	//uncomment this block for printing output (testing)
+	
+	printf("returned array: (");
+	int j = 0;
+	for (j=0; j<6; j++) {
+		printf("%.7g ", ret_arr[j]);
+	}
+	printf(")\n");
+	pfit(ret_arr);
+	*/
+	PyObject * ret_dict = Py_BuildValue("{s:d,s:d,s:d,s:d,s:d,s:d}", "c", ret_arr[0], "h", ret_arr[1], "b", ret_arr[2], "t", ret_arr[3], "p", ret_arr[4], "i", ret_arr[5]);
+	return ret_dict;
+}
+
+// COPY OF fit4pdrc but with fit3d
+static PyObject* bot_const_fit4pdrc(PyObject* self, PyObject* args) {
+	// first test with a list of responses for a set doses list
+	PyObject *doseList;
+	PyObject *doseItem;
+	PyObject *resList;
+	PyObject *resItem;
+	Py_ssize_t n;
+	Py_ssize_t dose_n;
+	Py_ssize_t res_n;
+	int i;
+
+	if (!PyArg_ParseTuple(args, "O!O!", &PyList_Type, &doseList, &PyList_Type, &resList)) {
+    	PyErr_SetString(PyExc_TypeError, "parameter must be a list of doses, followed by a list of responses.");
+    	return NULL;
+	}
+	dose_n = PyList_Size(doseList);
+	res_n = PyList_Size(resList);
+	if (dose_n != res_n) {
+		PyErr_SetString(PyExc_ValueError, "the dose and response list must be of equal size");
+		return NULL;
+	}
+	n = dose_n;
+	double * dose = malloc(sizeof(double) * dose_n);
+	double * res = malloc(sizeof(double) * res_n);
+	for (i=0; i<n; i++) {
+		doseItem = PyList_GetItem(doseList, i);
+		resItem = PyList_GetItem(resList, i);
+
+		if(PyFloat_Check(resItem)) {
+			res[i] = PyFloat_AsDouble(resItem);
+		} else if(PyLong_Check(resItem)) {
+			res[i] = PyFloat_AsDouble(resItem);
+		//} else if(resItem == Py_None) {
+		//	continue;
+		} else {
+			PyErr_SetString(PyExc_TypeError, "list items must be numeric.");
+        	return NULL;
+		}
+		if(PyFloat_Check(doseItem)) {
+			dose[i] = PyFloat_AsDouble(doseItem);
+		} else if(PyLong_Check(doseItem)) {
+			dose[i] = PyLong_AsDouble(doseItem);
+		} else {
+			PyErr_SetString(PyExc_TypeError, "list items must be numeric.");
+        	return NULL;
+		}
+	}
+	double ret_arr[6];
+	fit3d(res, dose, n, ret_arr);
+	free(res);
+	free(dose);
+	/*
+	//uncomment this block for printing output (testing)
+	
+	printf("returned array: (");
+	int j = 0;
+	for (j=0; j<6; j++) {
+		printf("%.7g ", ret_arr[j]);
+	}
+	printf(")\n");
+	pfit(ret_arr);
+	*/
+	PyObject * ret_dict = Py_BuildValue("{s:d,s:d,s:d,s:d,s:d,s:d}", "c", ret_arr[0], "h", ret_arr[1], "b", ret_arr[2], "t", ret_arr[3], "p", ret_arr[4], "i", ret_arr[5]);
+	return ret_dict;
+}
+
+// COPY OF fit4pdrc but with fit4d
+static PyObject* both_const_fit4pdrc(PyObject* self, PyObject* args) {
+	// first test with a list of responses for a set doses list
+	PyObject *doseList;
+	PyObject *doseItem;
+	PyObject *resList;
+	PyObject *resItem;
+	Py_ssize_t n;
+	Py_ssize_t dose_n;
+	Py_ssize_t res_n;
+	int i;
+
+	if (!PyArg_ParseTuple(args, "O!O!", &PyList_Type, &doseList, &PyList_Type, &resList)) {
+    	PyErr_SetString(PyExc_TypeError, "parameter must be a list of doses, followed by a list of responses.");
+    	return NULL;
+	}
+	dose_n = PyList_Size(doseList);
+	res_n = PyList_Size(resList);
+	if (dose_n != res_n) {
+		PyErr_SetString(PyExc_ValueError, "the dose and response list must be of equal size");
+		return NULL;
+	}
+	n = dose_n;
+	double * dose = malloc(sizeof(double) * dose_n);
+	double * res = malloc(sizeof(double) * res_n);
+	for (i=0; i<n; i++) {
+		doseItem = PyList_GetItem(doseList, i);
+		resItem = PyList_GetItem(resList, i);
+
+		if(PyFloat_Check(resItem)) {
+			res[i] = PyFloat_AsDouble(resItem);
+		} else if(PyLong_Check(resItem)) {
+			res[i] = PyFloat_AsDouble(resItem);
+		//} else if(resItem == Py_None) {
+		//	continue;
+		} else {
+			PyErr_SetString(PyExc_TypeError, "list items must be numeric.");
+        	return NULL;
+		}
+		if(PyFloat_Check(doseItem)) {
+			dose[i] = PyFloat_AsDouble(doseItem);
+		} else if(PyLong_Check(doseItem)) {
+			dose[i] = PyLong_AsDouble(doseItem);
+		} else {
+			PyErr_SetString(PyExc_TypeError, "list items must be numeric.");
+        	return NULL;
+		}
+	}
+	double ret_arr[6];
+	fit1d(res, dose, n, ret_arr);
+	free(res);
+	free(dose);
+	/*
+	//uncomment this block for printing output (testing)
+	
+	printf("returned array: (");
+	int j = 0;
+	for (j=0; j<6; j++) {
+		printf("%.7g ", ret_arr[j]);
+	}
+	printf(")\n");
+	pfit(ret_arr);
+	*/
+	PyObject * ret_dict = Py_BuildValue("{s:d,s:d,s:d,s:d,s:d,s:d}", "c", ret_arr[0], "h", ret_arr[1], "b", ret_arr[2], "t", ret_arr[3], "p", ret_arr[4], "i", ret_arr[5]);
+	return ret_dict;
+}
+
+
 // function Definition struct
 static PyMethodDef myMethods[] = {
 	{"fit4pdrc",fit4pdrc, METH_VARARGS, "fits a four parameter dose response curve"},
+	{"top_const_fit4pdrc",top_const_fit4pdrc, METH_VARARGS, "fits a dose response curve with the top constrained"},
+	{"bot_const_fit4pdrc",bot_const_fit4pdrc, METH_VARARGS, "fits a dose response curve with the bot constrained"},
+	{"both_const_fit4pdrc",both_const_fit4pdrc, METH_VARARGS, "fits a dose response curve with top and bot constrained"},
 	{NULL,NULL,0,NULL}
 };
 
--- pydrc/pydrc.py
+++ pydrc/pydrc.py
@@ -4,13 +4,20 @@ from math import log10
 sample_dose = [-7.52,-7,-6.52,-6,-5.52,-5,-4]
 sample_res = [0,1.3,10.2,43.3,75.1,91.5,100]
 
-def fit4pdrc(dose, response, power=False):
+def fit4pdrc(dose, response, power=False, top_const=False, bot_const=False):
     """
     takes a list of doses and responses and produces dictionary of return values
     """
     if type(power) is int:
         dose = M_to_log(dose, power)
-    ret = _pydrc.fit4pdrc(dose, response)
+    if (top_const and bot_const):
+        ret = _pydrc.both_const_fit4pdrc(dose, response)
+    elif top_const:
+        ret = _pydrc.top_const_fit4pdrc(dose, response)
+    elif bot_const:
+        ret = _pydrc.bot_const_fit4pdrc(dose, response)
+    else:
+        ret = _pydrc.fit4pdrc(dose, response)
     ret["dose"] = dose
     ret["response"] = response
     return ret
